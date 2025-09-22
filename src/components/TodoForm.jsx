2b37952914593774ba284c0af30d5a2da7d20cfd
Domizu1 <savadumancic2@gmail.com>
2025-09-22 18:09:42 +0200

    toggle added

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import moonIcon from "../assets/icon-moon.svg"
+import sunIcon from "../assets/icon-sun.svg"
 import { v4 as uuidv4 } from 'uuid';
 
 const TodoForm = ({ setTodos }) => {
     const [todo, setTodo] = useState("")
+    const [theme, setTheme] = useState("light")
 
-
+    function toggleTheme() {
+        setTheme(prev => (prev === "light" ? "dark" : "light"))
+    }
     const newTodo = {
         id: uuidv4(),
         name: todo,
@@ -27,7 +31,12 @@ const TodoForm = ({ setTodos }) => {
         <div className="max-w-[540px] w-[90%] mx-auto mt-[-150px] lg:mt-[-250px]">
             <div className="flex justify-between items-center">
                 <h1 className="uppercase text-white font-bold text-3xl lg:text-5xl tracking-[15px]">Todo</h1>
-                <img className="cursor-pointer" src={moonIcon} alt="" />
+                <img
+                    src={theme === "dark" ? sunIcon : moonIcon}
+                    onClick={toggleTheme}
+                    className="cursor-pointer w-6 h-6"
+                    alt="Toggle theme"
+                />
             </div>
             <form onSubmit={handleSubmit} className="mt-4 lg:mt-10">
                 <input value={todo} onChange={(e) => setTodo(e.target.value)} className="px-5 py-4 lg:px-6 lg:py-5 bg-white rounded-[5px] w-full outline-none" type="text" placeholder="Create a new todo…" />
